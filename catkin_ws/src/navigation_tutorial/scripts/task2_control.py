3fac8588de703d7513c96d7d1ca9880e0ad91d47
LisaYAMAGUCHI <lisa-381271z@g.ecc.u-tokyo.ac.jp>
2023-08-25 15:47:06 +0900

    画像5枚撮って移動する

--- catkin_ws/src/navigation_tutorial/scripts/task2_control.py
+++ catkin_ws/src/navigation_tutorial/scripts/task2_control.py
@@ -1,6 +1,8 @@
 from simple_control2 import simple_controller
 from three_dimensions_tutorial.scripts.keypoint_rcnn import judgement
+from three_dimensions_tutorial.scripts import take_image
 import rospy
+import cv2
 
 
 step = 0
@@ -16,31 +18,40 @@ if __name__=='__main__':
 
 if step == 1:
     ##写真を撮る
-    images = []
-    images.append()
+    imgsub = take_image.ImageSubscriber()
+    images = imgsub.get_image()
     step = 2
 
 if step == 2:
-    ##写真を切り取る？
+    ##写真を切り取る
     cut_images = []
     for i in range(len(images)):
-        cut_images.append(images[i])
+        # 画像読み込み
+        image = cv2.imread(images[i])
+        # img[top : bottom, left : right]
+        # サンプルの切り出し
+        cut_image = image[0 : 480, 320: 960]
+        cut_images.append(cut_image)
     step = 3
 
 if step == 3:
     ##手を振っている人を判断
-    judge = []
+    judges = []
     for j in range(len(cut_images)):
-        judge.append(judgement(cut_images[j]))
-    if judge == "None":
-        pass
-    else :
-        step = 4
+        judges.append(judgement(cut_images[j]))
+    
+    step = 4
 
 if step == 4:
-    if judge == "left":
-        simple_controller.turn_left(30)
-        simple_controller.go_straight(3.0)
-    else :
-        simple_controller.turn_right(30)
-        simple_controller.go_straight(3.0)
+    if all(i == judges[0] for i in judges):#もし全ての要素が同じだったら
+
+        if judges[0] == "left":
+            simple_controller.turn_left(30)
+            simple_controller.go_straight(3.0)
+        elif judges[0] == "right":
+            simple_controller.turn_right(30)
+            simple_controller.go_straight(3.0)
+        else :
+            step = 3
+    else:
+        step = 3
---
+++ catkin_ws/src/three_dimensions_tutorial/scripts/take_image.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import os
+
+class ImageSubscriber:
+    def __init__(self):
+        rospy.init_node('image_saver', anonymous=True)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber('camera/color/image_raw', Image, self.image_callback)
+        self.image_counter = 0
+        self.output_folder = 'saved_images'
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+        self.save_image = True
+
+    def image_callback(self, data):
+        if self.save_image:
+            try:
+                # ROS ImageメッセージをOpenCVの画像に変換
+                self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            except Exception as e:
+                rospy.logerr(e)
+                return
+            
+            """# 画像を保存
+            image_filename = os.path.join(self.output_folder, f"image_{self.image_counter}.jpg")
+            cv2.imwrite(image_filename, cv_image)
+            self.image_counter += 1
+            rospy.loginfo(f"Saved image: {image_filename}")"""
+        
+    def get_image(self):
+        images = []
+        for i in range(5):
+            images.append(self.cv_image)
+            rospy.sleep(1.0)
+        return images
+
+"""if __name__ == '__main__':
+    
+    image_subscriber = ImageSubscriber()
+    
+    while not rospy.is_shutdown():
+        user_input = input("Press Enter to save the next image, or type 'exit' to quit: ")
+        
+        if user_input.lower() == 'exit':
+            break
+        elif user_input == '':
+            image_subscriber.save_image = not image_subscriber.save_image
+    
+    rospy.signal_shutdown("Image saving program terminated.")
+"""
